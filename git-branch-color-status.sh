3982155937d0a4de13143d7b13fd0f5061cc54d2
Rafael Marinaro Verona <rverona@uolinc.com>
2019-10-03 16:58:15 -0300

    Fix bug where colors would not display correctly

--- git-branch-color-status.sh
+++ git-branch-color-status.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-COLOR_RED="\e[0;31m"
-COLOR_YELLOW="\e[0;33m"
-COLOR_GREEN="\e[0;32m"
-COLOR_OCHRE="\e[38;5;95m"
-COLOR_RESET="\e[0m"
+COLOR_RED="\001\033[0;31m\002"
+COLOR_YELLOW="\001\033[0;33m\002"
+COLOR_GREEN="\001\033[0;32m\002"
+COLOR_OCHRE="\001\033[38;5;95m\002"
+COLOR_RESET="\001\033[0m\002"
 
 isGitFolder() {
     git rev-parse --git-dir > /dev/null 2>&1
@@ -66,13 +66,13 @@ getGitBranchColor() {
   git_status="$(git status 2> /dev/null)"
 
   if [[ ! $git_status =~ "working tree clean" ]]; then
-    echo -e $COLOR_RED
+    echo $COLOR_RED
   elif [[ $git_status =~ "Your branch is ahead of" ]]; then
-    echo -e $COLOR_YELLOW
+    echo $COLOR_YELLOW
   elif [[ $git_status =~ "nothing to commit" ]]; then
-    echo -e $COLOR_GREEN
+    echo $COLOR_GREEN
   else
-    echo -e $COLOR_OCHRE
+    echo $COLOR_OCHRE
   fi
 }
 
