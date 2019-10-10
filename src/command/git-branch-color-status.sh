d4974ac3c317af8f3d4af7172c6c59636fca3a8d
Rafael Marinaro Verona <rverona@uolinc.com>
2019-10-10 17:01:30 -0300

    Fix code smells

--- src/command/git-branch-color-status.sh
+++ src/command/git-branch-color-status.sh
@@ -19,7 +19,7 @@ getGitBranch() {
 }
 
 getGitBranchRemote() {
-    git \for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD)
+    git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)"
 }
 
 getGitBranchAndStatus() {
@@ -29,13 +29,13 @@ getGitBranchAndStatus() {
 }
 
 parseGitStatus() {
-	status=`git status 2>&1`
-	dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
-	untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
-	ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
-	newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
-	renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
-	deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+	status=$(git status 2>&1)
+	dirty=$(echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?")
+	untracked=$(echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?")
+	ahead=$(echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?")
+	newfile=$(echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?")
+	renamed=$(echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?")
+	deleted=$(echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?")
 	bits=''
 	if [ "${renamed}" == "0" ]; then
 		bits=">${bits}"
@@ -66,18 +66,18 @@ getGitBranchColor() {
   git_status="$(git status 2> /dev/null)"
 
   if [[ ! $git_status =~ "working tree clean" ]]; then
-    echo $COLOR_RED
+    echo "$COLOR_RED"
   elif [[ $git_status =~ "Your branch is ahead of" ]]; then
-    echo $COLOR_YELLOW
+    echo "$COLOR_YELLOW"
   elif [[ $git_status =~ "nothing to commit" ]]; then
-    echo $COLOR_GREEN
+    echo "$COLOR_GREEN"
   else
-    echo $COLOR_OCHRE
+    echo "$COLOR_OCHRE"
   fi
 }
 
 if isGitFolder; then
     if isNotEmptyRepository; then 
-        echo -e $(getGitBranchColor)$(getGitBranchAndStatus)$COLOR_RESET;
+        echo -e "$(getGitBranchColor)$(getGitBranchAndStatus)$COLOR_RESET";
     fi
 fi
