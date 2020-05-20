2aa140ae77d22f0fca1d94ae4aab9bd5dc09ee4b
Rafael Verona <raverona@gmail.com>
2020-05-20 15:41:06 -0300

    Add class to represent the git status of the current directory

---
+++ test/git/Status.spec.js
@@ -0,0 +1,25 @@
+const {expect} = require('chai');
+const sinon = require('sinon');
+
+const Status = require("../../src/git/Status");
+const shell = require('shelljs');
+
+describe('Status', function () {
+    let status = new Status();
+    let gitFakeStatus = "On branch master\n" +
+        "Your branch is up to date with 'origin/master'.\n" +
+        "\n" +
+        "nothing to commit, working tree clean";
+
+    before(function () {
+        sinon.stub(shell, "exec").withArgs(`git status 2> /dev/null`, {silent: true}).callsFake(() => ({
+            stdout: gitFakeStatus
+        }))
+    });
+
+    describe('#status()', function () {
+        it('should return the status of the branch on the current directory', function () {
+            expect(status.status()).to.be.equal(gitFakeStatus)
+        });
+    });
+});
