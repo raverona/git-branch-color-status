8dde44d632de31bb4f7c96ee4a1b582309282480
Rafael Verona <raverona@gmail.com>
2020-05-22 17:28:52 -0300

    Add class to represent the current branch information

--- test/git/Status.spec.js
+++ test/git/Status.spec.js
@@ -11,15 +11,19 @@ describe('Status', function () {
         "\n" +
         "nothing to commit, working tree clean";
 
-    before(function () {
-        sinon.stub(shell, "exec").withArgs(`git status 2> /dev/null`, {silent: true}).callsFake(() => ({
-            stdout: gitFakeStatus
-        }))
-    });
-
     describe('#status()', function () {
+        before(function () {
+            sinon.stub(shell, "exec").withArgs(`git status 2> /dev/null`, {silent: true}).callsFake(() => ({
+                stdout: gitFakeStatus
+            }))
+        });
+
         it('should return the status of the branch on the current directory', function () {
             expect(status.status()).to.be.equal(gitFakeStatus)
+        });
+
+        after(function () {
+            sinon.restore()
         });
     });
 });
