afc23e6364fb78e4070963fdd4ee71f08bf2fa98
Rafael Verona <raverona@gmail.com>
2020-05-23 01:32:23 -0300

    Add class to get every status processor symbol

---
+++ src/git/symbol/Symbols.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+class Symbols {
+    constructor(...statusProcessors) {
+        this.statusProcessors = statusProcessors;
+    }
+
+    printAll() {
+        return this.statusProcessors.map(
+            (statusProcessor) =>
+                statusProcessor.getSymbol()
+        ).reduce(
+            (acc, curr) =>
+                acc + curr
+        );
+    }
+}
+
+module.exports = Symbols;
---
+++ test/git/symbol/Symbols.spec.js
@@ -0,0 +1,17 @@
+const {expect} = require('chai');
+
+const Symbols = require("../../../src/git/symbol/Symbols");
+
+describe('Symbols', function () {
+    let symbols = new Symbols(
+        {getSymbol: () => "symbol1"},
+        {getSymbol: () => "symbol2"},
+        {getSymbol: () => "symbol3"}
+    );
+
+    describe('#printAll()', function () {
+        it('should return the concatenation of the call of every status processor getSymbol function', function () {
+            expect(symbols.printAll()).to.be.equal("symbol1symbol2symbol3")
+        });
+    });
+});
